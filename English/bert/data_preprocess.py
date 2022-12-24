55b317bc94ee907c84dbc17645c498b95d1cc46e
ZhijiaCHEN <zhijia.chen@temple.edu>
2022-12-24 15:31:09 -0500

    .

--- English/bert/data_preprocess.py
+++ English/bert/data_preprocess.py
@@ -9,43 +9,9 @@ from torch.utils.data import DataLoader
 from attack_KernelGAT import load_KernelGAT
 from models import Pretrained_Fever_BERT
 
-
-# def generate_embdding(tokenizer, model, data):
-#     word_to_embeddings = {}
-    
-#     for x in tqdm(data, total=len(data)):
-#         indexed_tokens = tokenizer.encode(x,add_special_tokens=False)
-#         token_tensor = torch.tensor([indexed_tokens]).to(device)
-#         tokenized_words = [tokenizer._convert_id_to_token(x) for x in indexed_tokens]
-#         with torch.no_grad():
-#             output = model(token_tensor, output_hidden_states=True)
-#         last_word = ''
-#         embeddings = []
-#         for i in range(len(indexed_tokens)):
-#             word = tokenized_words[i]
-#             embed = output['hidden_states'][0][0][i].cpu()
-#             if len(word) > 2 and word[:2] == '##':
-#                 last_word += word[2:]
-#                 embeddings.append(embed)
-#             else:
-#                 if len(embeddings) > 0:
-#                     word_to_embeddings.setdefault(last_word, []).append(torch.mean(torch.stack(embeddings), 0))
-#                 last_word = word
-#                 embeddings = [embed]
-
-#     word_embedding = []
-#     word_list = []
-#     for word in sorted(word_to_embeddings.keys()):
-#         word_embedding.extend(word_to_embeddings[word])
-#         word_list.extend([word] * len(word_to_embeddings[word]))
-#     word_embedding = torch.stack(word_embedding)
-#     # joblib.dump(data, "fever_data.pkl")
-#     joblib.dump(word_list, '/'.join(args.test_data.split('/')[:-1]) + "/word-list.pkl")
-#     torch.save(word_embedding, '/'.join(args.test_data.split('/')[:-1]) + "/word-embedding.pt")
-
 def generate_embdding(tokenizer, model, data):
+    # 产生 word到embedding的映射，每个word在不同的句子下有不同的embedding，所以每个单词对应一个list of embeddings
     word_to_embeddings = {}
-    
     for x in tqdm(data, total=len(data)):
         indexed_tokens = tokenizer.encode(x, add_special_tokens=True)
         token_tensor = torch.tensor([indexed_tokens]).to(device)
@@ -55,13 +21,16 @@ def generate_embdding(tokenizer, model, data):
         last_word = ''
         embeddings = []
         for i in range(len(indexed_tokens)):
+            # 每个单词有个可能被拆分为多个token, ##预示着当前的token属于上一个单词的一部分
             word = tokenized_words[i]
             embed = output[0][0][i].cpu()
             if len(word) > 2 and word[:2] == '##':
+                
                 last_word += word[2:]
                 embeddings.append(embed)
             else:
                 if len(embeddings) > 0:
+                    # 我们对每个单词取其所有token的embedding的平均值作为该单词的embedding
                     word_to_embeddings.setdefault(last_word, []).append(torch.mean(torch.stack(embeddings), 0))
                 last_word = word
                 embeddings = [embed]
@@ -90,6 +59,7 @@ if __name__ == "__main__":
     tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
     device = torch.device("cuda")
     model_states = torch.load(args.model_states)
+    # 根据输入的参数加载对应模型，获取数据的word embedding
     if args.model_name == 'bert':
         model = Pretrained_Fever_BERT(model_states)
     elif args.model_name == 'kgat':
